fcb590fc825d5997426baaff132c50d230334174
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-10-28 19:31:19 +0530

    Time: 1 ms (16.73%), Space: 40.8 MB (22.11%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -16,8 +16,38 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root==null) return true;
-        return recursive(root.left, root.right);
+        // return recursive(root.left, root.right);
+        return iterative(root.left, root.right);
     } 
+
+    boolean iterative(TreeNode left, TreeNode right) {
+        Queue<TreeNode> qLeft = new LinkedList<>();
+        Queue<TreeNode> qRight = new LinkedList<>();
+        if(left!=null) qLeft.add(left);
+        if(right!=null) qRight.add(right);
+        while(!qLeft.isEmpty() || !qRight.isEmpty()) {
+            int sizeLeft = qLeft.size();
+            int sizeRight = qRight.size();
+            if(sizeLeft!=sizeRight) return false;
+            while(sizeLeft-->0) {
+                TreeNode currLeft = qLeft.remove();
+                TreeNode currRight = qRight.remove();
+                if(currLeft.val!=currRight.val) return false;
+
+                if(currLeft.left==null && currRight.right!=null) return false;
+                if(currLeft.right==null && currRight.left!=null) return false;
+                if(currLeft.left!=null && currRight.right==null) return false;
+                if(currLeft.right!=null && currRight.left==null) return false;
+
+                if(currLeft.left!=null) qLeft.add(currLeft.left);
+                if(currLeft.right!=null) qLeft.add(currLeft.right);
+                if(currRight.right!=null) qRight.add(currRight.right);
+                if(currRight.left!=null) qRight.add(currRight.left);
+            }
+        }
+
+        return true;
+    }
 
     boolean recursive(TreeNode left, TreeNode right) {
         if(left==null && right==null) return true;
