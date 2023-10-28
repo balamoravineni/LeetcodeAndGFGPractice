2ada7797bce4455b2d3afa86e4aa22f9e5420a93
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-10-28 19:35:12 +0530

    Time: 1 ms (16.73%), Space: 40.5 MB (48.53%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -34,10 +34,8 @@ class Solution {
                 TreeNode currRight = qRight.remove();
                 if(currLeft.val!=currRight.val) return false;
 
-                if(currLeft.left==null && currRight.right!=null) return false;
-                if(currLeft.right==null && currRight.left!=null) return false;
-                if(currLeft.left!=null && currRight.right==null) return false;
-                if(currLeft.right!=null && currRight.left==null) return false;
+                if((currLeft.left==null || currRight.right==null) && currLeft.left!=currRight.right) return false;
+                if((currLeft.right==null || currRight.left==null) && currLeft.right!=currRight.left) return false;
 
                 if(currLeft.left!=null) qLeft.add(currLeft.left);
                 if(currLeft.right!=null) qLeft.add(currLeft.right);
