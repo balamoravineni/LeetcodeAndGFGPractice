2925111f978379f9f04f3ae836a5ad98bc1a21a5
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-11-26 19:34:27 +0530

    Time: 11 ms (84.57%), Space: 41.5 MB (61.43%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -12,11 +12,33 @@ class Solution {
     Node targetNode = null;
     
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        Node newRoot = createNewTree(root, null, target);
-        // preorder(newRoot);
-        List<Integer> ans = new ArrayList<>();
-        recursiveDistance(targetNode, k, 0, ans);
+       
+        List<Integer> ans = new ArrayList<>(); 
+        // Node newRoot = createNewTree(root, null, target);
+        // // preorder(newRoot);
+        // recursiveDistance(targetNode, k, 0, ans);
+        
+        Map<TreeNode, TreeNode> parents = new HashMap<>();
+        preorder(root, null, parents);
+        Map<TreeNode, Boolean> visited = new HashMap<>();
+        recursiveDistance(target, parents, visited, k, 0, ans);
         return ans;
+    }
+
+    void recursiveDistance(TreeNode root, Map<TreeNode, TreeNode> parents, Map<TreeNode, Boolean> visited, int k, int distance, List<Integer> ans)  {
+        if(root==null || visited.get(root)!=null && visited.get(root)==true) return;
+        visited.put(root, true);
+        if(distance==k) {ans.add(root.val); return;}
+        recursiveDistance(root.left, parents, visited, k, distance+1, ans);
+        recursiveDistance(root.right, parents, visited, k, distance+1, ans);
+        if(parents.get(root)!=null) recursiveDistance(parents.get(root), parents, visited, k, distance+1, ans);
+    }
+
+    void preorder(TreeNode root, TreeNode parent, Map<TreeNode, TreeNode> parents) {
+        if(root==null) return;
+        parents.put(root, parent);
+        preorder(root.left, root, parents);
+        preorder(root.right, root, parents);
     }
 
     void recursiveDistance(Node root, int k, int distance, List<Integer> ans)  {
