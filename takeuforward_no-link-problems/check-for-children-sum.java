ebcc36922bc0b0d4c934792fee4d7a2cc9a1759d
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-11-26 12:38:14 +0530

    Update check-for-children-sum.java

--- takeuforward_no-link-problems/check-for-children-sum.java
+++ takeuforward_no-link-problems/check-for-children-sum.java
@@ -13,44 +13,42 @@ class TreeNode {
 }
 
 class Solution {
-    boolean incrementSum(TreeNode root) {
-        if(root==null) return false;
-        int leftVal = 0, rightVal = 0;
-        boolean current = false;
-        if(root.left!=null) leftVal = root.left.val;
-        if(root.right!=null) rightVal = root.right.val;
-        if(root.val<leftVal+rightVal) {
-            root.val = leftVal+rightVal;
-            current = true;
+    void childrenSum(TreeNode root) {
+        // while(incrementSum(root));
+        if(root==null) return;
+        int sum = 0;
+        if(root.left!=null) sum += root.left.val;
+        if(root.right!=null) sum += root.right.val;
+        if(root.val<sum) root.val = sum;
+        if(root.val>sum) {
+            if(root.left!=null) root.left.val = root.val;
+            if(root.right!=null) root.right.val = root.val;
         }
-        boolean left = incrementSum(root.left);
-        boolean right = incrementSum(root.right);
-        return current||left||right;
-    }
-    
-    void recursive(TreeNode root) {
-        while(incrementSum(root));
+        childrenSum(root.left);
+        childrenSum(root.right);
+        sum = 0;
+        if(root.left!=null) sum += root.left.val;
+        if(root.right!=null) sum += root.right.val;
+        if(root.val<sum) root.val = sum;
     }
 }
 
 class GFG {
 	public static void main (String[] args) {
 		System.out.println("childrenSum of a Binary Tree");
-		TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(10);
-        root.left.left.right = new TreeNode(5);
-        // root.left.left.right.right = new TreeNode(6);
-        root.right = new TreeNode(3);
-        root.right.left = new TreeNode(9);
-        root.right.right = new TreeNode(10);
+		TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(35);
+        root.right = new TreeNode(10);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(2);
 
         System.out.print("Original Preorder: ");
         preorder(root);
         System.out.println();
         
-        new Solution().recursive(root);
+        new Solution().childrenSum(root);
         
         System.out.print("Modified Preorder: ");
         preorder(root);
