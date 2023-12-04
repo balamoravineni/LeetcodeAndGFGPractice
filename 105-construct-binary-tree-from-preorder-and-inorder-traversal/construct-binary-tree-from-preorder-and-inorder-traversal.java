b91314781e322d3c8e045b09502cc43a41a13e0e
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-04 21:16:26 +0530

    Time: 3 ms (44.25%) | Memory: 43.3 MB (54.52%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,14 +15,34 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length==0) return null;
-        int current = preorder[0];
+        return buildTreeHelper(preorder, inorder, 0, preorder.length-1, 0, inorder.length-1);
+        // if(preorder.length==0) return null;
+        // int current = preorder[0];
+        // TreeNode root = new TreeNode(current);
+        // if(preorder.length==1) return root;
+        // int currentInorderIndex = findIndex(current, inorder);
+        // root.left = buildTree(Arrays.copyOfRange(preorder, 1, currentInorderIndex+1), Arrays.copyOfRange(inorder, 0, currentInorderIndex));
+        // root.right = buildTree(Arrays.copyOfRange(preorder, currentInorderIndex+1, preorder.length), Arrays.copyOfRange(inorder, currentInorderIndex+1, inorder.length));
+        // return root;
+    }
+
+    TreeNode buildTreeHelper(int[] preorder, int[] inorder, int preStart, int preEnd, int inStart, int inEnd) {
+        if(preStart>preEnd) return null;
+        int current = preorder[preStart];
         TreeNode root = new TreeNode(current);
-        if(preorder.length==1) return root;
-        int currentInorderIndex = findIndex(current, inorder);
-        root.left = buildTree(Arrays.copyOfRange(preorder, 1, currentInorderIndex+1), Arrays.copyOfRange(inorder, 0, currentInorderIndex));
-        root.right = buildTree(Arrays.copyOfRange(preorder, currentInorderIndex+1, preorder.length), Arrays.copyOfRange(inorder, currentInorderIndex+1, inorder.length));
+        if(preEnd==preStart) return root;
+        int currentInorderIndex = findIndexHelper(current, inorder, inStart, inEnd);
+        int lenLeftTree = currentInorderIndex-inStart;
+        root.left = buildTreeHelper(preorder, inorder, preStart+1, preStart+lenLeftTree, inStart, currentInorderIndex-1);
+        root.right = buildTreeHelper(preorder, inorder, preStart+lenLeftTree+1, preEnd, currentInorderIndex+1, inEnd);
         return root;
+    }
+
+    int findIndexHelper(int current, int[] inorder, int inStart, int inEnd) {
+        for(int i=inStart;i<=inEnd; i++) {
+            if(inorder[i]==current) return i;
+        }
+        return -1;
     }
     
     int findIndex(int current, int[] inorder) {
