33ddafab623660c7f5de8d74d293d8a331721648
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-18 18:45:11 +0530

    Time: 13 ms (7.62%) | Memory: 44.6 MB (20.64%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -1,6 +1,32 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        Arrays.sort(nums);
-        return (nums[nums.length-1]*nums[nums.length-2]) - (nums[0]*nums[1]);
+        // Arrays.sort(nums);
+        // return (nums[nums.length-1]*nums[nums.length-2]) - (nums[0]*nums[1]);
+        int largest = Integer.MIN_VALUE;
+        int lI = -1;
+        int smallest = Integer.MAX_VALUE;
+        int sI = -1;
+        for(int i=0; i<nums.length; i++) {
+            if(largest<nums[i]) {
+                largest = nums[i];
+                lI = i;
+            }
+            if(smallest>nums[i]) {
+                smallest = nums[i];
+                sI = i;
+            }
+        }
+        int second_largest = Integer.MIN_VALUE;
+        int second_smallest = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length; i++) {
+            if(i!=lI && second_largest<=nums[i]) {
+                second_largest = nums[i];
+            }
+            if(i!=sI && second_smallest>=nums[i]) {
+                second_smallest = nums[i];
+            }
+        }
+        System.out.println(largest + " " + second_largest + " " + smallest + " " + second_smallest);
+        return (largest*second_largest) - (smallest*second_smallest);
     }
 }
