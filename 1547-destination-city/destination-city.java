a12933859d5f2c61a40e6fa39c978e3c0ebe35a3
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-19 12:26:24 +0530

    Time: 2 ms (76.05%) | Memory: 43.4 MB (23.70%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -4,8 +4,8 @@ class Solution {
         for(List<String> cities: paths) {
             pathMap.put(cities.get(0), cities.get(1));
         }
-        for(String key: pathMap.keySet()) {
-            if(pathMap.get(pathMap.get(key))==null) return pathMap.get(key);
+        for(String value: pathMap.values()) {
+            if(pathMap.get(value)==null) return value;
         }
         return "";
     }
