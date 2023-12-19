4bfecef1ff210fe1e49b6f2c4fbadd6912435eeb
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-19 09:14:29 +0530

    Time: 5 ms (48.07%) | Memory: 41.5 MB (98.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] map = new int[30];
-        for(int i=0;i<s.length();i++) map[s.charAt(i)-'a']++;
-        for(int i=0;i<t.length();i++) map[t.charAt(i)-'a']--;
-        for(int i=0;i<30;i++) if(map[i]!=0) return false;
+        if(s.length()!=t.length()) return false;
+        int[] map = new int[26];
+        for(int i=0;i<s.length();i++) {
+            map[s.charAt(i)-'a']++;
+            map[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++) if(map[i]!=0) return false;
         return true;
     }
 }
