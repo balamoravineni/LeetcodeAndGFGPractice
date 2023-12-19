5d40bf7b379e1db3350184b5c5e4ee0b265d2918
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-19 13:12:45 +0530

    Time: 7 ms (50.42%) | Memory: 44.6 MB (46.78%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -1,9 +1,71 @@
 class Solution {
 
+
+    public int[][] imageSmoother(int[][] img) {
+        // return solution(img);
+        return spaceOptimizedSoln(img);
+    }
+
+    int[][] spaceOptimizedSoln(int[][] img) {
+        int rows = img.length, columns = img[0].length;
+        int[] temp = new int[columns];
+        int[] current = new int[columns];
+        int prevNeighbor = 0;
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<columns;j++) current[j] = img[i][j];
+            for(int j=0;j<columns;j++) {
+                int sum = 0, num = 0;
+                // bottom neighbors
+                if(i+1<rows) {
+                    if(j-1>=0) {
+                        sum += img[i+1][j-1];
+                        num++;
+                    }
+                    sum += img[i+1][j];
+                    num++;
+                    if(j+1<columns) {
+                        sum += img[i+1][j+1];
+                        num++;
+                    }
+                }
+                // next neighbor
+                if(j+1<columns) {
+                    sum += img[i][j+1];
+                    num++;
+                }
+                //current cell
+                sum += img[i][j];
+                num++;
+                // prevNeighbor
+                if(j-1>=0) {
+                    sum += prevNeighbor;
+                    num++;
+                }
+                // top neighbors
+                if(i-1>=0) {
+                    if(j-1>=0) {
+                        sum += temp[j-1];
+                        num++;
+                    }
+                    sum += temp[j];
+                    num++;
+                    if(j+1<columns) {
+                        sum += temp[j+1];
+                        num++;
+                    }
+                }
+                prevNeighbor = img[i][j];
+                img[i][j] = sum/num;
+            }
+            for(int j=0;j<columns;j++) temp[j] = current[j];
+        }
+        return img;
+    }
+
     int[] dx = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
     int[] dy = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
 
-    public int[][] imageSmoother(int[][] img) {
+    int[][] solution(int[][] img) {
         int rows = img.length, columns = img[0].length;
         int[][] ans = new int[rows][columns];
         for(int i=0;i<rows;i++) {
