bac9919f411c890e730f4cd73368d2d21e9dfbce
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-23 08:00:18 +0530

    Time: 1 ms (96.48%) | Memory: 42 MB (7.05%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -1,27 +1,27 @@
 
 class Solution {
     public boolean isPathCrossing(String path) {
-        Set<Pair> pathSet = new HashSet<>();
-        Pair current = new Pair(0,0);
+        Set<Pair<Integer, Integer>> pathSet = new HashSet<>();
+        Pair<Integer, Integer> current = new Pair(0,0);
         pathSet.add(current);
         // System.out.println(pathSet);
         for(int i=0;i<path.length();i++) {
-            Pair next = null;
+            Pair<Integer, Integer> next = null;
             switch(path.charAt(i)) {
                 case 'N': {
-                    next = new Pair(current.getKey(), (int)current.getValue()+1);
+                    next = new Pair(current.getKey(), current.getValue()+1);
                     break;
                 }
                 case 'S': {
-                    next = new Pair(current.getKey(), (int)current.getValue()-1);
+                    next = new Pair(current.getKey(), current.getValue()-1);
                     break;
                 }
                 case 'E': {
-                    next = new Pair((int)current.getKey()+1, current.getValue());
+                    next = new Pair(current.getKey()+1, current.getValue());
                     break;
                 }
                 case 'W': {
-                    next = new Pair((int)current.getKey()-1, current.getValue());
+                    next = new Pair(current.getKey()-1, current.getValue());
                     break;
                 }
             }
