25eb48e4df60ed7d8f5f549166052862ad717c3a
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-24 10:12:31 +0530

    Time: 3 ms (14.80%) | Memory: 44.6 MB (6.34%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -17,26 +17,17 @@ class Solution {
     List<Integer> ans = new ArrayList<>();
     public void recoverTree(TreeNode root) {
         inorder(root);
-        System.out.println(ans);
-        List<Integer> copy = new ArrayList<>(ans);
-        Collections.sort(copy);
-        Integer r1 = 0, r2 = 0;
-        for(int i=0;i<ans.size();i++) {
-            if(ans.get(i)!=copy.get(i)) {
-                r1 = ans.get(i);
-                r2 = copy.get(i);
-                break;
-            }
-        }
-        System.out.println(r1 + " " + r2);
-        preorderReplace(root,r1,r2);
+        // System.out.println(ans);
+        Collections.sort(ans);
+        // System.out.println(r1 + " " + r2);
+        inorderReplace(root,new int[1]);
     }
-    void preorderReplace(TreeNode root, int r1, int r2) {
+    void inorderReplace(TreeNode root, int[] i) {
         if(root==null) return;
-        if(root.val==r1) root.val = r2;
-        else if(root.val==r2) root.val = r1;
-        preorderReplace(root.left, r1, r2);
-        preorderReplace(root.right, r1, r2);
+        inorderReplace(root.left, i);
+        if(root.val!=ans.get(i[0])) root.val = ans.get(i[0]);
+        i[0]++;
+        inorderReplace(root.right, i);
     }
 
     void inorder(TreeNode root) {
