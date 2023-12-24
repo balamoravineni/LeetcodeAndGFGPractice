7b4f7576f647b99d41404f0aedc5aabd09ebda4b
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-24 10:43:57 +0530

    Time: 2 ms (29.25%) | Memory: 44.3 MB (6.34%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,14 +14,49 @@
  * }
  */
 class Solution {
-    List<Integer> ans = new ArrayList<>();
     public void recoverTree(TreeNode root) {
+        // usingExtraSpace(root);
+        while(inorderSort(root, null, null));
+    }
+
+    boolean inorderSort(TreeNode root, TreeNode min, TreeNode max) {
+        if(root==null) return false;
+        if(root.left!=null && root.left.val>root.val) {
+            swap(root, root.left);
+            return true;
+        }
+        if(root.right!=null && root.right.val<root.val) {
+            swap(root, root.right);
+            return true;
+        }
+        if(min!=null && min.val>root.val) {
+            swap(root, min);
+            return true;
+        }
+        if(max!=null && max.val<root.val) {
+            swap(root, max);
+            return true;
+        }
+        boolean left = inorderSort(root.left, min, root);
+        boolean right = inorderSort(root.right, root, max);
+        return left || right;
+    }
+
+    void swap(TreeNode t1, TreeNode t2) {
+        int temp = t1.val;
+        t1.val = t2.val;
+        t2.val = temp;
+    }
+
+    List<Integer> ans = new ArrayList<>();
+    void usingExtraSpace(TreeNode root) {
         inorder(root);
-        // System.out.println(ans);
+        System.out.println(ans);
         Collections.sort(ans);
         // System.out.println(r1 + " " + r2);
         inorderReplace(root,new int[1]);
     }
+
     void inorderReplace(TreeNode root, int[] i) {
         if(root==null) return;
         inorderReplace(root.left, i);
