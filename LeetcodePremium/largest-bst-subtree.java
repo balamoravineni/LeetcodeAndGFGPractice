5cc408116f4f5c153b11f641c2b361853ed86c63
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-24 14:00:21 +0530

    Update largest-bst-subtree.java
    previous solution was wrong

--- LeetcodePremium/largest-bst-subtree.java
+++ LeetcodePremium/largest-bst-subtree.java
@@ -1,77 +1,60 @@
 /*
   Leetcode link: https://leetcode.com/problems/largest-bst-subtree/
+  GfG link: https://practice.geeksforgeeks.org/problems/largest-bst/1
   youtube link: https://www.youtube.com/watch?v=X0oXMdtUDwo
 */
+//User function Template for Java
 
-/*package whatever //do not write package name here */
+// class Node  
+// { 
+//     int data; 
+//     Node left, right; 
+   
+//     public Node(int d)  
+//     { 
+//         data = d; 
+//         left = right = null; 
+//     } 
+// }
 
-import java.io.*;
-import java.util.*;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int val) {
-        this.val = val;
+class Trio {
+    long nodeMin, nodeMax;
+    int maxSize;
+    Trio(long a, long b, int c) {
+        nodeMin = a;
+        nodeMax = b;
+        maxSize = c;
     }
 }
 
-class Solution {
+
+class Solution{
     
-    TreeNode largestBSTinBT(TreeNode root) {
-        helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
-        return ans; // or ansSize depending on the question
+    // Return the size of the largest sub-tree which is also a BST
+    
+    static int largestBst(Node root)
+    {
+        // Write your code here
+        Trio ans = helper(root);
+        return ans.maxSize; // or ansSize depending on the question
     }
     
-    TreeNode ans;
-    int ansSize = 0;
-    int helper(TreeNode root, long min, long max) {
-        if(root==null) return 0;
-        int left = helper(root.left, min, root.val);
-        int right = helper(root.right, root.val, max);
-        if(left==-1 || right==-1) return -1;
-        else if(root.val>min && root.val<max){
-            int temp = 1+left+right;   
-            if(temp>ansSize) {
-                ans = root;
-                ansSize = temp;
-            }
-            return temp;
+    static Trio helper(Node root) {
+        if(root==null) return new Trio(Long.MAX_VALUE, Long.MIN_VALUE, 0);
+        Trio left = helper(root.left);
+        Trio right = helper(root.right);
+        if(root.data<right.nodeMin && root.data>left.nodeMax) {
+            return new Trio(
+                Math.min(root.data, left.nodeMin),
+                Math.max(root.data, right.nodeMax),
+                1 + left.maxSize + right.maxSize
+            );
         }
-        return -1;
+        return new Trio(
+            Long.MIN_VALUE,
+            Long.MAX_VALUE,
+            Math.max(left.maxSize, right.maxSize)
+        );
     }
-}
-
-class GFG {
-	public static void main (String[] args) {
-		System.out.println("Binary Search Tree -> Largest BST in BT");
-		TreeNode root = new TreeNode(4);
-        root.left = new TreeNode(5);
-        root.right = new TreeNode(15);
-        root.left.left = new TreeNode(1);
-        root.left.right = new TreeNode(8);
-        root.right.left = new TreeNode(14);
-        root.right.right = new TreeNode(17);
-        root.right.right.right = new TreeNode(19);
-
-        System.out.print("Original inorder: ");
-        inorder(root);
-        System.out.println();
-        
-        TreeNode largestBST = new Solution().largestBSTinBT(root);
-        
-        System.out.print("largest BST: ");
-        inorder(largestBST);
-        System.out.println();
-        
-	}
     
-    static void inorder(TreeNode root) {
-        if(root==null) return;
-        inorder(root.left);
-        System.out.print(root.val + " ");
-        inorder(root.right);
-    }
-
 }
