778fde6e6c1b9115ca8bca944c2a48b63f2e3278
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-24 08:11:09 +0530

    Time: 2 ms (95.20%) | Memory: 45.6 MB (5.09%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -14,11 +14,47 @@
  * }
  */
 class Solution {
-    Set<Integer> set = new HashSet<>();
+
+    Deque<TreeNode> nextStack = new ArrayDeque<>();
+    Deque<TreeNode> previousStack = new ArrayDeque<>();
+
+    int next() {
+        TreeNode current = nextStack.pop();
+        pushAllLeft(current.right);
+        return current.val;
+    }
+
+    int previous() {
+        TreeNode current = previousStack.pop();
+        pushAllRight(current.left);
+        return current.val;
+    }
+
+    void pushAllLeft(TreeNode root) {
+        while(root!=null) {
+            nextStack.push(root);
+            root = root.left;
+        }
+    }
+
+    void pushAllRight(TreeNode root) {
+        while(root!=null) {
+            previousStack.push(root);
+            root = root.right;
+        }
+    }
+
     public boolean findTarget(TreeNode root, int k) {
-        if(root==null) return false;
-        if(set.contains(k-root.val)) return true;
-        set.add(root.val);
-        return findTarget(root.left,k) || findTarget(root.right,k);
+        pushAllLeft(root);
+        pushAllRight(root);
+
+        int start = next();
+        int end = previous();
+        while(start+end!=k && start!=end) {
+            if(start+end<k) start = next();
+            else end = previous();
+        }
+        if(start==end) return false;
+        return true;
     }
 }
