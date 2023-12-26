e4d55059449dc9d87b5cef6d46c52676390c1105
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2023-12-26 12:09:23 +0530

    Time: 1 ms (84.17%) | Memory: 42 MB (8.00%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,7 +1,26 @@
 class Solution {
-    Map<String,Integer> dpMap = new HashMap<>();
 
     public int numDecodings(String s) {
+        // return amateurCode(s);
+        int[] dp = new int[s.length()+1];
+        Arrays.fill(dp,-1);
+        return recursiveHelper(s,0,dp);
+    }
+
+    int recursiveHelper(String s, int index, int[] dp) {
+        if(index==s.length()) return 1;
+        if(s.charAt(index)=='0') return 0;
+        if(dp[index]!=-1) return dp[index];
+        int ans = recursiveHelper(s, index+1, dp);
+        if(index+2<=s.length() && Integer.parseInt(s.substring(index, index+2))<=26) {
+            ans += recursiveHelper(s, index+2, dp);
+        }
+        return dp[index]=ans;
+    }
+
+
+    Map<String,Integer> dpMap = new HashMap<>();
+    int amateurCode(String s) {
         if(s.charAt(0)=='0') return 0;
         else if(s.length()==1) return 1;
         else if(s.length()==2 && (s.charAt(1)=='0' && s.charAt(0)>'2')) return 0;
