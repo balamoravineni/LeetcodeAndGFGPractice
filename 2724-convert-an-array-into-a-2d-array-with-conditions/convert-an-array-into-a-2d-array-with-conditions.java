8084ea86d7f53091a3b5f85fcd089110819aaceb
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-02 09:58:56 +0530

    Time: 29 ms (5.30%) | Memory: 44.9 MB (18.64%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,5 +1,30 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
+        // return sortingWay(nums);
+        return usingMap(nums);
+    }   
+
+    List<List<Integer>> usingMap(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int highFreq = 1;
+        for(int temp: nums) {
+            if(map.get(temp)==null) map.put(temp,1);
+            else {
+                map.put(temp,map.get(temp)+1);
+                highFreq = Math.max(highFreq, map.get(temp));
+            }
+        }
+        System.out.println(map + " " + highFreq);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<highFreq;i++) ans.add(new ArrayList<>());
+        for(int key: map.keySet()) {
+            int freq = map.get(key);
+            while(--freq>=0) ans.get(freq).add(key);
+        }
+        return ans;
+    }
+
+    List<List<Integer>> sortingWay(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
         int index = 0;
