027c9e80980c541d42776f107a9d633ab53d26b1
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-02 14:36:44 +0530

    Time: 1 ms (68.84%) | Memory: 44.6 MB (31.52%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -3,9 +3,25 @@ class Solution {
         int[] visited = new int[graph.length];
         for(int i=0;i<graph.length;i++) {
             if(visited[i]==0) {
-                if(!bfs(graph, i, visited)) {
+                // if(!bfs(graph, i, visited)) {
+                //     return false;
+                // }
+                if(!dfs(graph, i, visited, 1)) {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+
+    boolean dfs(int[][] graph, int index, int[] visited, int currLevel) {
+        visited[index] = currLevel;
+        for(int i=0;i<graph[index].length;i++) {
+            if(visited[graph[index][i]]==0) {
+                if(!dfs(graph, graph[index][i], visited, currLevel==1?2:1)) return false;
+            }
+            else if(visited[graph[index][i]]==currLevel) {
+                return false;
             }
         }
         return true;
