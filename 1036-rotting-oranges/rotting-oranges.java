5cb601f46e95861a95644e2a6b22da73132bda6c
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-03 17:53:02 +0530

    Time: 2 ms (84.20%) | Memory: 42.1 MB (33.05%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -3,24 +3,20 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
         Queue<Pair<Integer,Integer>> init = new LinkedList<>();
+        int count = 0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 if(grid[i][j]==2) init.add(new Pair(i,j));
+                else if(grid[i][j]==1) count++;
             }
         }
-        int ans = bfs(grid,n,m,init);
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(grid[i][j]==1) return -1;
-            }
-        }
-        return ans;
+        return bfs(grid,n,m,init,count);
     }
 
     int[] x = {0, 1, 0, -1};
     int[] y = {1, 0, -1, 0};
 
-    int bfs(int[][] grid, int n, int m, Queue<Pair<Integer,Integer>> queue) {
+    int bfs(int[][] grid, int n, int m, Queue<Pair<Integer,Integer>> queue, int count) {
         int level = -1;
         while(!queue.isEmpty()) {
             int size = queue.size();
@@ -32,12 +28,13 @@ class Solution {
                     if(nextX>=0 && nextX<n && nextY>=0 && nextY<m &&
                         grid[nextX][nextY]==1) {
                         grid[nextX][nextY] = 2;
+                        count--;
                         queue.add(new Pair(nextX, nextY));
                     }
                 }
             }
             level++;
         }
-        return level==-1?0:level;
+        return count!=0?-1:level==-1?0:level;
     }
 }
