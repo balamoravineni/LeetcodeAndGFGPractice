f2c422fee78880648759c0f7a85b77e0ea6e099a
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-11 20:19:17 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (17.76%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,6 +2,19 @@ class Solution {
     public int search(int[] nums, int target) {
         // int ans = Arrays.binarySearch(nums, target);
         // return ans<0?-1:ans;
+        // return iterative(nums, target);
+        return recursive(nums, target, 0, nums.length-1);
+    }
+
+    int recursive(int[] nums, int target, int lo, int hi) {
+        if(lo>hi) return -1;
+        int mid = lo + (hi-lo)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]>target) return recursive(nums, target, lo, mid-1);
+        return recursive(nums, target, mid+1, hi);
+    }
+
+    int iterative(int[] nums, int target) {
         int lo = 0, hi = nums.length-1;
         while(lo<=hi) {
             int mid = lo + (hi-lo)/2;
