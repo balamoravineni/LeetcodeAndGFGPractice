bf51f21d8c44efd3a5fd850e48ed8ebbe4133ae3
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-12 09:05:09 +0530

    Time: 11 ms (17.80%) | Memory: 42 MB (17.05%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -3,13 +3,20 @@ class Solution {
         int count = 0;
         int i = 0;
         s = s.toLowerCase();
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
         while(i<s.length()) {
-            if(i>=s.length()/2 && (s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u')) {
+            // if(i>=s.length()/2 && (s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u')) {
+            //     count--;
+            // }
+            if(i>=s.length()/2 && set.contains(s.charAt(i))) {
                 count--;
             }
-            else if(i<s.length()/2 && (s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u')) {
+            else if(i<s.length()/2 && set.contains(s.charAt(i))) {
                 count++;
             }
+            // else if(i<s.length()/2 && (s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u')) {
+            //     count++;
+            // }
             i++;
         }
         return count==0;
