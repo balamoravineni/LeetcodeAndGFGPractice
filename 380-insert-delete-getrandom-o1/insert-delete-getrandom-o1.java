7be455139298c0ee7603366648ca3b7a1ca45e31
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-16 19:15:41 +0530

    Time: 48 ms (41.64%) | Memory: 93.5 MB (20.84%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -1,41 +1,56 @@
+// commented code is initial code, implemented optimised one in uncommented code
 class RandomizedSet {
 
-    int count = 0;
-    Map<Integer,Integer> mapCountToVal = new HashMap<>();
-    Map<Integer,Integer> mapValToCount = new HashMap<>();
-    Queue<Integer> removedCount = new LinkedList<>();
+    // int count = 0;
+    // Map<Integer,Integer> mapCountToVal = new HashMap<>();
+    // Map<Integer,Integer> mapValToCount = new HashMap<>();
+    // Queue<Integer> removedCount = new LinkedList<>();
     Random random = new Random();
+    List<Integer> list = new LinkedList<>();
+    Map<Integer,Integer> mapValToIndex = new HashMap<>();
 
     public RandomizedSet() {
         
     }
     
     public boolean insert(int val) {
-        if(mapValToCount.containsKey(val)) return false;
-        int temp = count;
-        if(!removedCount.isEmpty()) temp = removedCount.remove();
-        else count++;
-        mapValToCount.put(val, temp);
-        mapCountToVal.put(temp, val);
+        // if(mapValToCount.containsKey(val)) return false;
+        // int temp = count;
+        // if(!removedCount.isEmpty()) temp = removedCount.remove();
+        // else count++;
+        // mapValToCount.put(val, temp);
+        // mapCountToVal.put(temp, val);
+        // return true;
+        if(mapValToIndex.containsKey(val)) return false;
+        list.add(val);
+        mapValToIndex.put(val, list.size()-1);
         return true;
     }
     
     public boolean remove(int val) {
-        if(!mapValToCount.containsKey(val)) return false;
-        int temp = mapValToCount.get(val);
-        mapValToCount.remove(val);
-        mapCountToVal.remove(temp);
-        removedCount.add(temp);
+        // if(!mapValToCount.containsKey(val)) return false;
+        // int temp = mapValToCount.get(val);
+        // mapValToCount.remove(val);
+        // mapCountToVal.remove(temp);
+        // removedCount.add(temp);
+        // return true;
+        if(!mapValToIndex.containsKey(val)) return false;
+        int index = mapValToIndex.get(val);
+        list.set(index, list.get(list.size()-1));
+        mapValToIndex.put(list.get(list.size()-1), index);
+        list.remove(list.size()-1);
+        mapValToIndex.remove(val);
         return true;
     }
     
     public int getRandom() {
-        int number = -1;
-        while(true) {
-            number = random.nextInt(count);
-            if(mapCountToVal.containsKey(number)) break;
-        }
-        return mapCountToVal.get(number);
+        // int number = -1;
+        // while(true) {
+        //     number = random.nextInt(count);
+        //     if(mapCountToVal.containsKey(number)) break;
+        // }
+        // return mapCountToVal.get(number);
+        return list.get(random.nextInt(list.size()));
     }
 }
 
