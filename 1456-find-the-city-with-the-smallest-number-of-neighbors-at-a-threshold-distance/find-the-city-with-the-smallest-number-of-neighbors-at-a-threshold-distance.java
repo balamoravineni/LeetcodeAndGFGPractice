e56c169adaa3c758158cfb702008fe1933832b10
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-16 19:56:31 +0530

    Time: 10 ms (65.25%) | Memory: 44.1 MB (58.13%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,5 +1,42 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        // return usingDjikstra(n, edges, distanceThreshold);
+        return usingFloydWarshall(n, edges, distanceThreshold);
+    }
+
+    int usingFloydWarshall(int n, int[][] edges, int distanceThreshold) {
+        int[][] dist = new int[n][n];
+        for(int[] temp:dist) Arrays.fill(temp, Integer.MAX_VALUE);
+        for(int i=0;i<n;i++) dist[i][i] = 0;
+        for(int[] edge: edges) {
+            dist[edge[0]][edge[1]] = edge[2];
+            dist[edge[1]][edge[0]] = edge[2];
+        }
+        for(int k=0;k<n;k++) {
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<n;j++) {
+                    if(dist[i][k]!=Integer.MAX_VALUE && dist[k][j]!=Integer.MAX_VALUE && 
+                        dist[i][j] > dist[i][k] + dist[k][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+        int ans = -1;
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++) {
+            int count = 0;
+            for(int j=0;j<n;j++) {
+                if(dist[i][j]<=distanceThreshold) count++;
+            }
+            if(count<=min) {
+                ans = i;
+                min = count;
+            }
+        }
+        return ans;
+    }
+
+    int usingDjikstra(int n, int[][] edges, int distanceThreshold) {
         List<List<Pair<Integer,Integer>>> adjList = new ArrayList<>();
         for(int i=0;i<n;i++) adjList.add(new ArrayList<>());
         for(int[] edge: edges) {
