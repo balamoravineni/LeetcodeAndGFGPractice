f3bad2745f3f500aec92073e0673cd6588eb6aaf
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-17 14:26:28 +0530

    Time: 2 ms (98.10%) | Memory: 42.4 MB (5.39%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,5 +1,22 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
+        // return usingMapAndSet(arr);
+        return usingArrays(arr);
+    }
+
+    boolean usingArrays(int[] arr) {
+        int[] counter = new int[2001];
+        boolean[] freq = new boolean[1001];
+        for(int temp: arr) counter[temp+1000]++;
+        for(int temp: counter) {
+            if(temp==0) continue;
+            if(freq[temp]) return false;
+            else freq[temp] = true;
+        }
+        return true;
+    }
+
+    boolean usingMapAndSet(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int temp: arr) {
             if(map.containsKey(temp)) map.put(temp, map.get(temp)+1);
