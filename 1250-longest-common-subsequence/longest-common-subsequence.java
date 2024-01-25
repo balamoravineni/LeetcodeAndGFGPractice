3713a37beb7f7b75f96b142f6a5e6778b3967d64
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-25 23:02:58 +0530

    Time: 30 ms (22.44%) | Memory: 50.8 MB (42.31%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -7,8 +7,8 @@ class Solution {
 
     int helper(String text1, String text2, int i1, int i2, int[][] dp) {
         if(i1<0 || i2<0) return 0;
-        if(dp[i1][i2]!=-1) return dp[i1][i2];
         if(text1.charAt(i1)==text2.charAt(i2)) return 1 + helper(text1, text2, i1-1, i2-1, dp);
+        if(dp[i1][i2]!=-1) return dp[i1][i2];
         int poss1 = helper(text1, text2, i1-1, i2, dp);
         int poss2 = helper(text1, text2, i1, i2-1, dp);
         return dp[i1][i2] = Math.max(poss1, poss2);
