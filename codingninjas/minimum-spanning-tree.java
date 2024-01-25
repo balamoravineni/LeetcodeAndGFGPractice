0c1112b3c31b0dd8cbe83182c21f23cddc89f6a0
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-25 09:25:49 +0530

    Update minimum-spanning-tree.java

--- codingninjas/minimum-spanning-tree.java
+++ codingninjas/minimum-spanning-tree.java
@@ -24,10 +24,87 @@ class Tuple {
 		parent = c;
 	}
 }
+
+
+class DisjointSet {
+    
+    List<Integer> parents = new ArrayList<>();
+    List<Integer> ranks = new ArrayList<>();
+    List<Integer> sizes = new ArrayList<>();
+    
+    DisjointSet(int n) {
+        for(int i=0;i<=n;i++) {
+            parents.add(i);
+            ranks.add(0);
+            sizes.add(1);
+        }
+    }
+    
+    public int findUParent(int node) {
+        if(node==parents.get(node)) return node;
+        int parent = parents.get(node);
+        int ulParent = findUParent(parent);
+        parents.set(node, ulParent);
+        return parents.get(node);
+    }
+    
+    public void unionByRank(int node1, int node2) {
+        int ul_par1 = findUParent(node1);
+        int ul_par2 = findUParent(node2);
+        if(ul_par1==ul_par2) return;
+        if(ranks.get(ul_par1)>ranks.get(ul_par2)) {
+            parents.set(ul_par2, ul_par1);
+        }
+        else if(ranks.get(ul_par1)<ranks.get(ul_par2)) {
+            parents.set(ul_par1, ul_par2);
+        }
+        else {
+            parents.set(ul_par1, ul_par2);
+            ranks.set(ul_par2, ranks.get(ul_par2)+1);
+        }
+    }
+    
+    public void unionBySize(int node1, int node2) {
+        int ul_par1 = findUParent(node1);
+        int ul_par2 = findUParent(node2);
+        if(ul_par1==ul_par2) return;
+        if(sizes.get(ul_par1)>sizes.get(ul_par2)) {
+            parents.set(ul_par2, ul_par1);
+            sizes.set(ul_par1, sizes.get(ul_par2)+sizes.get(ul_par1));
+        }
+        else { // covers both less than or equal for sizes
+            parents.set(ul_par1, ul_par2);
+            sizes.set(ul_par2, sizes.get(ul_par2)+sizes.get(ul_par1));
+        }
+    }
+}
+
+
 public class Solution {
 
 	public static int minimumSpanningTree(ArrayList<ArrayList<Integer>> edges, int n) {
 		//Your code goes here
+		// return usingPrimsAlgo(edges,n);
+		return usingKruskalsAlgo(edges, n);
+	}
+
+	static int usingKruskalsAlgo(ArrayList<ArrayList<Integer>> edges, int n) {
+		DisjointSet ds = new DisjointSet(n);
+		Collections.sort(edges, (e1, e2) -> e1.get(2)-e2.get(2));
+		int ans = 0;
+		for(ArrayList<Integer> edge: edges) {
+			int node1 = edge.get(0);
+			int node2 = edge.get(1);
+			int weight = edge.get(2);
+			if(ds.findUParent(node1)!=ds.findUParent(node2)) {
+				ans += weight;
+				ds.unionBySize(node1, node2);
+			}
+		}
+		return ans;
+	}
+
+	static int usingPrimsAlgo(ArrayList<ArrayList<Integer>> edges, int n) {
 		List<List<Pair>> adjList = new ArrayList<>();
 		for(int i=0;i<n;i++) adjList.add(new ArrayList<>());
 		for(ArrayList<Integer> edge: edges) {
@@ -35,8 +112,9 @@ public class Solution {
 			adjList.get(edge.get(1)).add(new Pair(edge.get(0), edge.get(2)));
 
 		}
-		return primsalgo(adjList,n)
+		return primsalgo(adjList,n);
 	}
+
 	static int primsalgo(List<List<Pair>> adjList, int n) {
 		boolean[] visited = new boolean[n];
 		PriorityQueue<Tuple> pq = new PriorityQueue<>((t1, t2) -> t1.weight-t2.weight);
