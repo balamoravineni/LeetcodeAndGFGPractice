3e8690b88a0ae0a5f17f644ff34312e2a876a0f3
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-26 11:13:19 +0530

    Create bubble-sort.java

---
+++ codingninjas/bubble-sort.java
@@ -0,0 +1,23 @@
+/*
+  Problem Link: https://www.codingninjas.com/studio/problems/bubble-sort_624380
+  Description: 
+  You are given an integer array 'arr' of size 'N'.
+  You must sort this array using 'Bubble Sort'.
+*/
+
+public class Solution {
+    public static void bubbleSort(int[] arr, int n) {
+        //Your code goes here
+        for(int i=0;i<n-1;i++) {
+            // push the max to the last using adjacent swaps
+            for(int j=0;j<n-1-i;j++) {
+                if(arr[j+1]<arr[j]) { // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+}
