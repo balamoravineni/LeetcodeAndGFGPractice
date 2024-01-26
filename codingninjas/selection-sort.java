5151cde3e84d7d06e123b615c2e96a435da98714
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-26 11:02:04 +0530

    Create selection-sort.java

---
+++ codingninjas/selection-sort.java
@@ -0,0 +1,27 @@
+/*
+  Problem Link: https://www.codingninjas.com/studio/problems/selection-sort_624469
+  Description:
+  Sort the given unsorted array 'arr' of size 'N' in non-decreasing order using the selection sort algorithm.
+*/
+
+public class Solution {
+    public static void selectionSort(int[] arr) {
+        //Your code goes here
+        int n = arr.length;
+        for(int i=0;i<n-1;i++) { // select minimums (or maximums) in each step for non-decreasing order (or non-increasing order)
+            int min = arr[i];
+            int minIndex = i;
+            for(int j=i+1;j<n;j++) {
+                if(arr[j]<min) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            if(minIndex!=i) { //swap
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
