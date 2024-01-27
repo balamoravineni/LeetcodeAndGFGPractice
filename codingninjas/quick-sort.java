a59d7bb7e9278a521f948da3f39e1c68a16a2b6d
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-27 10:50:07 +0530

    Create quick-sort.java

---
+++ codingninjas/quick-sort.java
@@ -0,0 +1,33 @@
+/*
+  Problem Link: https://www.codingninjas.com/studio/problems/quick-sort_5844
+  Description:
+  Given the 'start' and the 'end'' positions of the array 'input'. Your task is to sort the elements between 'start' and 'end' using quick sort.
+*/
+
+
+public class Solution {
+	
+	public static void quickSort(int[] input,int startIndex, int endIndex) {
+		if(startIndex>=endIndex) return;
+		int pivot = input[startIndex];
+		int i = startIndex+1, j = endIndex;
+		while(i<=j) {
+			while(i<=endIndex && input[i]<=pivot) i++;
+			while(j>=startIndex && input[j]>pivot) j--;
+			// System.out.println(i + " " + j);
+			if(i<j) swap(input, i, j);
+		}
+		// j -> pivotIndex
+		swap(input, startIndex, j);
+		// System.out.println("pivot index: " + j);
+		quickSort(input, startIndex, j-1);
+		quickSort(input, j+1, endIndex);
+	}
+
+	static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+}
