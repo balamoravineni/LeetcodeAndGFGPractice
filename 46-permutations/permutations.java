28428cbadadcbe802a3ea879ca6c03522b50d6a7
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-30 09:26:07 +0530

    Time: 1 ms (95.93%) | Memory: 44.2 MB (75.28%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        recursive(nums, new ArrayList<>(), visited, ans);
+        recursive(nums, 0, ans);
         return ans;
     }
 
-    void recursive(int[] nums, List<Integer> permutation,boolean[] visited, List<List<Integer>> ans) {
-        if(permutation.size()==nums.length) {
-            ans.add(new ArrayList(permutation));
+    void recursive(int[] nums, int index, List<List<Integer>> ans) {
+        if(index==nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for(int t: nums) temp.add(t);
+            ans.add(temp);
             return;
         }
-        for(int i=0;i<nums.length;i++) {
-            if(!visited[i]) {
-                permutation.add(nums[i]);
-                visited[i] = true;
-                recursive(nums, permutation, visited, ans);
-                permutation.remove(permutation.size()-1);
-                visited[i] = false;
-            }
+        for(int i=index;i<nums.length;i++) {
+            swap(nums,i,index);
+            recursive(nums, index+1, ans);
+            swap(nums,i,index);
         }
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
