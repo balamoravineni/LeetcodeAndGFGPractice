8fe6de21769cf15c44112824171eac3ec261336c
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-01-30 09:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (6.75%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,11 +1,13 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        recursive(nums, 0, ans);
+        // boolean[] visited = new boolean[nums.length];
+        // recursive(nums, new ArrayList<>(), visited, ans);
+        recursiveOptimised(nums, 0, ans);
         return ans;
     }
 
-    void recursive(int[] nums, int index, List<List<Integer>> ans) {
+    void recursiveOptimised(int[] nums, int index, List<List<Integer>> ans) {
         if(index==nums.length) {
             List<Integer> temp = new ArrayList<>();
             for(int t: nums) temp.add(t);
@@ -14,7 +16,7 @@ class Solution {
         }
         for(int i=index;i<nums.length;i++) {
             swap(nums,i,index);
-            recursive(nums, index+1, ans);
+            recursiveOptimised(nums, index+1, ans);
             swap(nums,i,index);
         }
     }
@@ -23,5 +25,21 @@ class Solution {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+        void recursive(int[] nums, List<Integer> permutation,boolean[] visited, List<List<Integer>> ans) {
+        if(permutation.size()==nums.length) {
+            ans.add(new ArrayList(permutation));
+            return;
+        }
+        for(int i=0;i<nums.length;i++) {
+            if(!visited[i]) {
+                permutation.add(nums[i]);
+                visited[i] = true;
+                recursive(nums, permutation, visited, ans);
+                permutation.remove(permutation.size()-1);
+                visited[i] = false;
+            }
+        }
     }
 }
