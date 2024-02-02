46bf7627216eb019b08c06b1c72110254d3294f4
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-02 09:27:10 +0530

    Time: 11 ms (6.81%) | Memory: 41.1 MB (20.94%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        
+        String init = Integer.toString(low);
+        String end = Integer.toString(high);
+
+        List<Integer> ans = new ArrayList<>();
+
+        System.out.println(init.length() + " -> " + end.length());
+
+
+        for(int i=init.length();i<=end.length() && i<=9;i++) {
+            StringBuilder start = new StringBuilder();
+            int temp = i;
+            for(int j=1;j<=temp;j++) start.append(j + "");
+            System.out.println(temp + ": " + start.toString());
+            while(true) {
+                Integer curr = new Integer(start.toString());
+                if(curr<low) {
+                    start.deleteCharAt(0);
+                    if(++temp>9) break;
+                    start.append(temp + "");
+                    continue;
+                }
+                else if(curr>high) break;
+                ans.add(curr);
+                start.deleteCharAt(0);
+                if(++temp>9) break;
+                start.append(temp + "");
+            }
+        }
+
+        return ans;
+    }
+}
