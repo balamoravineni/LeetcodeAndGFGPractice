db82e3cf3532654be2ae74973852111d92869fc1
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-05 21:46:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (19.31%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length==1) {
+            return nums[0]==target?0:-1;
+        }
+        int pivot = pivotPoint(nums, 0, nums.length-1);
+        // System.out.println("pivot: " + pivot);
+        int ans = -1;
+        if(pivot==-1) {
+            ans = Arrays.binarySearch(nums, target);
+        }
+        else if(target<=nums[pivot] && target>=nums[0]) {
+            ans = Arrays.binarySearch(nums, 0, pivot+1, target);
+        }
+        else {
+            ans = Arrays.binarySearch(nums, pivot+1, nums.length, target);
+        }
+        return ans<0?-1:ans;
+    }
+
+    int pivotPoint(int[] nums, int lo, int hi) {
+        if(lo>=hi || nums[lo]<nums[hi]) {
+            // System.out.println(lo + " " + hi + ": -1");
+            return -1;
+        }
+        else if(lo==hi-1 && nums[lo]>nums[hi]) {
+            // System.out.println(lo + " " + hi + ": " + lo);
+            return lo;
+        }
+        else if(lo==hi-1) {
+            // System.out.println(lo + " " + hi + ": -1");
+            return -1;
+        }
+        int mid = lo + (hi-lo)/2;
+        int left = pivotPoint(nums, lo, mid);
+        int right = pivotPoint(nums, mid+1, hi);
+        // System.out.println(lo + " " + hi + ": " + left + " -> " + right);
+        if(left==-1 && right==-1) return mid;
+        else if(left==-1) return right;
+        else if(right==-1) return left;
+        return -1;
+    }
+}
