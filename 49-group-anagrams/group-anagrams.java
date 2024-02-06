fe65eda0422d27cbdfaf916f5511ceacf1084c2d
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-06 22:05:31 +0530

    Time: 22 ms (14.13%) | Memory: 48.6 MB (10.91%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,5 +1,32 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        // return usingSorting(strs);
+        return usingSignature(strs);
+    }
+
+    List<List<String>> usingSignature(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for(String temp: strs) {
+            String signature = getSignature(temp);
+            if(map.containsKey(signature)) map.get(signature).add(temp);
+            else map.put(signature, new ArrayList<>(Arrays.asList(temp)));
+        }
+        List<List<String>> ans = new ArrayList<>();
+        for(String key: map.keySet()) ans.add(map.get(key));
+        return ans;
+    }
+
+    String getSignature(String str) {
+        int[] freq = new int[26];
+        for(int i=0;i<str.length();i++) freq[str.charAt(i)-'a']++;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<26;i++) {
+            if(freq[i]!=0) sb.append(i+"-"+freq[i]+"$");
+        }
+        return sb.toString();
+    }
+
+    List<List<String>> usingSorting(String[] strs) {
         Map<String,List<String>> map = new HashMap<>();
         for(String temp: strs) {
             char[] tempArr = temp.toCharArray();
