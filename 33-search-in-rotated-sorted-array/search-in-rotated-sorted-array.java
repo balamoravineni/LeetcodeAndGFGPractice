618b32057bc079245a02c0bd259b45213aef696b
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-06 09:38:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (89.27%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,8 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length==1) {
-            return nums[0]==target?0:-1;
+        // return usingPivotPoint(nums, target);
+        return usingBSDirectly(nums, target, 0, nums.length-1);
+    }
+
+    int usingBSDirectly(int[] nums, int target, int lo, int hi) {
+        if(lo>hi) return -1;
+        int mid = (lo+hi)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[lo]<nums[hi]) {
+            if(nums[mid]<target) {
+                return usingBSDirectly(nums, target, mid+1, hi);
+            }
+            else {
+                return usingBSDirectly(nums, target, lo, mid);
+            }
         }
+        else if(nums[lo]<=nums[mid]) {
+            if(nums[lo]<=target && target<=nums[mid]) {
+                return usingBSDirectly(nums, target, lo, mid);
+            }
+            else {
+                return usingBSDirectly(nums, target, mid+1, hi);
+            }
+        }
+        else if(nums[mid]<=nums[hi]) {
+            if(nums[mid]<=target && target<=nums[hi]) {
+                return usingBSDirectly(nums, target, mid, hi);
+            }
+            else {
+                return usingBSDirectly(nums, target, lo, mid-1);
+            }
+        }
+        return -1;
+    }
+
+    int usingPivotPoint(int[] nums, int target) {
         int pivot = pivotPoint(nums, 0, nums.length-1);
         // System.out.println("pivot: " + pivot);
         int ans = -1;
