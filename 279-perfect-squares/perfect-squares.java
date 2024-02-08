8bb2f2b1ade1b4a94bb0364f4de5de47a359bb7f
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-08 08:56:44 +0530

    Time: 66 ms (32.13%) | Memory: 43.3 MB (31.45%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,9 +1,9 @@
 class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n+1];
-        // Arrays.fill(dp, -1);
-        // return recursive(n, dp);
-        return iterative(n, dp);
+        Arrays.fill(dp, -1);
+        return recursive(n, dp);
+        // return iterative(n, dp);
     }
 
     int iterative(int n, int[] dp) {
@@ -23,7 +23,7 @@ class Solution {
         if(dp[n]!=-1) return dp[n];
         int ans = Integer.MAX_VALUE;
         for(int i=1;i*i<=n;i++) {
-            ans = Math.min(ans, 1 + numSquares(n-(i*i)));
+            ans = Math.min(ans, 1 + recursive(n-(i*i), dp));
         }
         return dp[n]=ans;
     }
