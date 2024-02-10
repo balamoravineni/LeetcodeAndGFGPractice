64ad07912b491b01dc9f61793af2a7b0aa51def0
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-10 08:20:12 +0530

    Time: 782 ms (5.05%) | Memory: 251.1 MB (5.04%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<=n;j++) {
+                String temp = s.substring(i,j);
+                if(isPalindrome(temp)) ans++;
+            }  
+        }
+        return ans;
+    }
+    
+    Map<String,Boolean> memo = new HashMap<>();
+
+    boolean isPalindrome(String curr) {
+        if(memo.containsKey(curr)) return memo.get(curr);
+        int i=0; 
+        int j=curr.length()-1;
+        while(i<j) {
+            if(curr.charAt(i)!=curr.charAt(j)) {
+                memo.put(curr, false);
+                return false;
+            }
+            i++;
+            j--;
+        }
+        memo.put(curr, true);
+        return true;
+    }
+}
