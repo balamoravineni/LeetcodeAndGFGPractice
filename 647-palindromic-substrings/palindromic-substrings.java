cab011208580e61ec2827d4f78366aa4a1c79aa7
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-10 22:21:11 +0530

    Time: 2 ms (96.29%) | Memory: 41.5 MB (61.97%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,5 +1,34 @@
 class Solution {
     public int countSubstrings(String s) {
+        // return bruteForce(s);
+        return optimised(s);
+    }
+
+    int optimised(String s) {
+        int ans = 0;
+        for(int i=0;i<s.length();i++) {
+            // single character is always palindrom
+            ans += 1;
+            // odd length palindromes possible with current index
+            ans += countPalindromes(s,i-1,i+1);
+            // even length palindromes possible with current index
+            ans += countPalindromes(s,i,i+1);
+        }
+        return ans;
+    }
+
+    int countPalindromes(String s, int left, int right) {
+        int count = 0;
+        while(left>=0 && right<=s.length()-1) {
+            if(s.charAt(left)==s.charAt(right)) count++;
+            else break;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+    int bruteForce(String s) {
         int n = s.length();
         int ans = 0;
         for(int i=0;i<n;i++) {
@@ -10,22 +39,17 @@ class Solution {
         }
         return ans;
     }
-    
-    // Map<String,Boolean> memo = new HashMap<>();
 
     boolean isPalindrome(String curr) {
-        // if(memo.containsKey(curr)) return memo.get(curr);
         int i=0; 
         int j=curr.length()-1;
         while(i<j) {
             if(curr.charAt(i)!=curr.charAt(j)) {
-                // memo.put(curr, false);
                 return false;
             }
             i++;
             j--;
         }
-        // memo.put(curr, true);
         return true;
     }
 }
