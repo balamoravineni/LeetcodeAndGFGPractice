94c0d0635a6e5938c9928b0192a56d174b7b2412
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-11 10:04:58 +0530

    Time: 3 ms (95.76%) | Memory: 44.4 MB (71.85%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,5 +1,31 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // return usingKahnsAlgo(numCourses, prerequisites);
+        return usingDFS(numCourses, prerequisites);
+    }
+
+    boolean usingDFS(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i=0;i<numCourses;i++) adjList.add(new ArrayList<>());
+        for(int i=0;i<prerequisites.length;i++) adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        int[] visited = new int[numCourses];
+        for(int i=0;i<numCourses;i++) {
+            if(visited[i]==0 && dfsIsCycle(adjList, visited, i)) return false;
+        }
+        return true;
+    }
+
+    boolean dfsIsCycle(List<List<Integer>> adjList, int[] visited, int index) {
+        visited[index] = 2;
+        for(int temp: adjList.get(index)) {
+            if(visited[temp]==2) return true;
+            if(visited[temp]==0 && dfsIsCycle(adjList, visited, temp)) return true;
+        }
+        visited[index] = 1;
+        return false;
+    }
+
+    boolean usingKahnsAlgo(int numCourses, int[][] prerequisites) {
         int[] inDegree = new int[numCourses];
         List<List<Integer>> adjList = new ArrayList<>();
         for(int i=0;i<numCourses;i++) adjList.add(new ArrayList<>());
