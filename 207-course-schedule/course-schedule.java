544088ef906db06e45a59f8b0305aafac5598180
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-11 09:55:01 +0530

    Time: 5 ms (73.06%) | Memory: 44.5 MB (66.09%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] inDegree = new int[numCourses];
         List<List<Integer>> adjList = new ArrayList<>();
         for(int i=0;i<numCourses;i++) adjList.add(new ArrayList<>());
-        for(int i=0;i<prerequisites.length;i++) adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
-        int[] visited = new int[numCourses];
-        for(int i=0;i<numCourses;i++) {
-            if(visited[i]==0 && dfsIsCycle(adjList, visited, i)) return false;
+        for(int i=0;i<prerequisites.length;i++) {
+            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            inDegree[prerequisites[i][0]]++;
         }
-        return true;
-    }
-
-    boolean dfsIsCycle(List<List<Integer>> adjList, int[] visited, int index) {
-        visited[index] = 2;
-        for(int temp: adjList.get(index)) {
-            if(visited[temp]==2) return true;
-            if(visited[temp]==0 && dfsIsCycle(adjList, visited, temp)) return true;
+        // for(int temp: inDegree) System.out.print(temp + " ");
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++) if(inDegree[i]==0) queue.add(i);
+        List<Integer> ans = new ArrayList<>();
+        while(!queue.isEmpty()) {
+            int curr = queue.remove();
+            ans.add(curr);
+            for(int temp: adjList.get(curr)) {
+                inDegree[temp]--;
+                if(inDegree[temp]==0) queue.add(temp);
+            }
         }
-        visited[index] = 1;
-        return false;
+        return ans.size()==numCourses;
     }
 }
