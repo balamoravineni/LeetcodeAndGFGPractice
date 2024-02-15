252a32b3f03840c46bc8213c6544040da8d0201b
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-15 07:46:20 +0530

    Time: 29 ms (69.56%) | Memory: 56.3 MB (72.31%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
@@ -15,6 +15,9 @@ class Solution {
                 sum -= nums[i];
                 sides--;
             }
+            else {
+                return sum;
+            }
         }
         if(sides<3) return -1;
         return sum;
