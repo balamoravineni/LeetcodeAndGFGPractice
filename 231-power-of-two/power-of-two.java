c6398bd863706126b70e8be5c0d8a92c89a92325
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-19 09:05:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (56.93%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,7 +1,17 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        // return recursive(n);
+        return usingBitManipulation(n);
+    }
+
+    boolean usingBitManipulation(int n) {
+        if(n<=0) return false;
+        return (n&(n-1))==0;
+    }
+
+    boolean recursive(int n) {
         if(n==0) return false;
         if(n==1) return true;
-        return n%2==0 && isPowerOfTwo(n/2);
+        return n%2==0 && recursive(n/2);
     }
 }
