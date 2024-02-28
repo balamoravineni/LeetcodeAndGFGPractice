0f0126d48e1813b92f4494504a320386188e6bc0
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-02-28 09:44:28 +0530

    Time: 3 ms (60.78%) | Memory: 54.2 MB (9.85%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,7 +1,22 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         // return usingBooleanArray(n, trust);
-        return usingMapAndSet(n, trust);
+        // return usingMapAndSet(n, trust);
+        return usingGraphConcept(n, trust);
+    }
+
+    int usingGraphConcept(int n, int[][] trust) {
+        int[] inDegree = new int[n];
+        int[] outDegree = new int[n];
+        for(int[] temp: trust) {
+            // works because given pairs are all unique
+            outDegree[temp[0]-1]++;
+            inDegree[temp[1]-1]++;
+        }
+        for(int i=0;i<n;i++) {
+            if(outDegree[i]==0 && inDegree[i]==n-1) return i+1;
+        }
+        return -1;
     }
 
     int usingMapAndSet(int n, int[][] trust) {
