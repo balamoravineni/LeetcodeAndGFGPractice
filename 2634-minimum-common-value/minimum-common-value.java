0eff846b25ed12286186de95f82483dfbc1a546a
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-03-09 08:43:11 +0530

    Time: 2 ms (81.56%) | Memory: 62.4 MB (80.15%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,5 +1,21 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
+        return usingTwoPointers(nums1, nums2);
+        // return usingBinarySearch(nums1, nums2);
+    }
+
+    int usingTwoPointers(int[] nums1, int[] nums2) {
+        int i=0;
+        int j=0;
+        while(i<nums1.length && j<nums2.length) {
+            if(nums1[i]==nums2[j]) return nums1[i];
+            else if(nums1[i]<nums2[j]) i++;
+            else j++;
+        }
+        return -1;
+    }
+
+    int usingBinarySearch(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
             return getCommon(nums2, nums1);
         }
