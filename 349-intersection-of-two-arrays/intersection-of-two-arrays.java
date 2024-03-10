a01decd5904448735f14726a1d9dd34928a6c39e
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-03-10 07:59:22 +0530

    Time: 3 ms (53.96%) | Memory: 43 MB (81.76%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,5 +1,36 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        // return usingMap(nums1,nums2);
+        return usingSorting(nums1, nums2);
+    }
+
+    int[] usingSorting(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0, j=0;
+        List<Integer> ans = new ArrayList<>();
+        while(i<nums1.length && j<nums2.length) {
+            if(nums1[i]==nums2[j]) ans.add(nums1[i]);
+            else if(nums1[i]<nums2[j]) {
+                i++;
+                continue;
+            }
+            else {
+                j++;
+                continue;
+            }
+            while(i+1<nums1.length && nums1[i+1]==nums1[i]) i++;
+            i++;
+            while(j+1<nums2.length && nums2[j+1]==nums2[j]) j++;
+            j++;
+        }
+        int[] res = new int[ans.size()];
+        int size = 0;
+        for(int temp: ans) res[size++] = temp;
+        return res;
+    }
+
+    int[] usingMap(int[] nums1, int[] nums2) {
         Map<Integer, Boolean> map = new HashMap<>();
         for(int temp: nums1) {
             map.put(temp, false);
