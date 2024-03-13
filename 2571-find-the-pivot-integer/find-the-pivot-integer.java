cc1b6914c45188e76f645e22c57d135c79d9186f
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-03-13 08:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (67.67%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,5 +1,17 @@
 class Solution {
     public int pivotInteger(int n) {
+        // return linearTime(n);
+        return optimised(n);
+    }
+
+    int optimised(int n) {
+        // x(x+1)/2 == n(n+1/2) - ((x-1)(x)/2) => x^2 = n(n+1)/2;
+        double ans = Math.sqrt(n*(n+1)/2);
+        if(ans-(int)ans==0) return (int)ans;
+        return -1;
+    }
+
+    int linearTime(int n) {
         int i=1;
         int j=n;
         int iSum = i;
