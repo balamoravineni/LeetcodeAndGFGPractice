29f8ca34feb18c47a4650b68d469e25b65a8003f
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-03-15 09:11:36 +0530

    Time: 2 ms (73.94%) | Memory: 53.5 MB (42.64%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -7,11 +7,11 @@ class Solution {
         }
         int prev = nums[nums.length-1];
         ans[nums.length-1] = ans[nums.length-2];
-        for(int i=nums.length-2;i>=0;i--) {
-            if(i!=0) ans[i] = ans[i-1]*prev;
-            else ans[i] = prev;
+        for(int i=nums.length-2;i>=1;i--) {
+            ans[i] = ans[i-1]*prev;
             prev = prev*nums[i];
         }
+        ans[0] = prev;
         return ans;
     }
 }
