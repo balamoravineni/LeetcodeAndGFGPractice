9c798466a91a26a84a85682965169432338d8008
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-03-17 14:52:59 +0530

    Time: 105 ms (42.90%) | Memory: 49.7 MB (18.89%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,67 +1,69 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        int beginWordIndex = -1;
-        int endWordIndex = -1;
-        for(int i=0;i<wordList.size();i++) {
-            if(wordList.get(i).equals(beginWord)) beginWordIndex = i;
-            if(wordList.get(i).equals(endWord)) endWordIndex = i;
-        }
-        if(endWordIndex==-1) return 0;
-        if(beginWordIndex==-1) {
-            wordList.add(beginWord);
-            beginWordIndex = wordList.size()-1;
-        }
-        int n = wordList.size();
+        Map<String,Integer> map = new HashMap<>();
         List<List<Integer>> adjList = new ArrayList<>();
-        for(int i=0;i<n;i++) adjList.add(new ArrayList<>());
-        for(int i=0;i<n-1;i++) {
-            for(int j=i+1;j<n;j++) {
-                if(isDiffOnlyOne(wordList.get(i), wordList.get(j))) {
-                    adjList.get(i).add(j);
-                    adjList.get(j).add(i);
+        for(int i=0;i<wordList.size();i++) {
+            map.put(wordList.get(i), i);
+            adjList.add(new ArrayList<>());
+        }
+        int endIndex = -1;
+        if(!map.containsKey(endWord)) return 0;
+        else endIndex = map.get(endWord);
+        int beginIndex = -1;
+        if(!map.containsKey(beginWord)) {
+            map.put(beginWord, adjList.size());
+            beginIndex = adjList.size();
+            wordList.add(beginWord);
+            adjList.add(new ArrayList<>());
+        }
+        else {
+            beginIndex = map.get(beginWord);
+        }
+        // System.out.println(beginIndex + " " + endIndex + ": " + wordList.size());
+        
+        for(String word: wordList) {
+            int currIndex = map.get(word);
+            char[] wordArr = word.toCharArray();
+            for(int i=0;i<wordArr.length;i++) {
+                char original = wordArr[i];
+                for(int temp=0;temp<26;temp++) {
+                    wordArr[i] = (char)((int)'a'+temp);
+                    String str = new String(wordArr);
+                    // System.out.println(str);
+                    if(map.containsKey(str)) {
+                        int strIndex = map.get(str);
+                        if(strIndex!=currIndex) {
+                            adjList.get(currIndex).add(strIndex);
+                            // adjList.get(strIndex).add(currIndex);
+                        }
+                    }
                 }
+                wordArr[i] = original;
             }
         }
-
         // System.out.println(adjList);
-        // System.out.println(endWordIndex);
-        // System.out.println(beginWordIndex);
-        return bfs(beginWordIndex, endWordIndex, adjList);
-    }
 
-    int bfs(int beginWordIndex, int endWordIndex, List<List<Integer>> adjList) {
-        boolean[] visited = new boolean[adjList.size()];
+        // BFS to get shortest path
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(beginWordIndex);
-        visited[beginWordIndex] = true;
-        int level = 0;
+        boolean[] visited = new boolean[adjList.size()];
+        queue.add(beginIndex);
+        visited[beginIndex] = true;
+        int ans = 1;
         while(!queue.isEmpty()) {
-            level++;
-            // System.out.println("level: " + level + " - " + queue);
-            int tempSize = queue.size();
-            for(int z=0;z<tempSize;z++) {
+            // System.out.println(ans + " -> " + queue);
+            int size = queue.size();
+            for(int i=0;i<size;i++) {
                 int curr = queue.remove();
-                // visited[curr] = true;
-                if(curr==endWordIndex) {
-                    return level;
-                }
-                for(int t: adjList.get(curr)) {
-                    if(!visited[t]) {
-                        queue.add(t);
-                        visited[t] = true;
+                if(curr==endIndex) return ans;
+                for(int temp: adjList.get(curr)) {
+                    if(!visited[temp]) {
+                        queue.add(temp);
+                        visited[temp] = true;
                     }
                 }
             }
+            ans++;
         }
         return 0;
-    }
-
-    boolean isDiffOnlyOne(String s1, String s2) {
-        int diff = 0;
-        for(int i=0;i<s1.length();i++) {
-            if(s1.charAt(i)!=s2.charAt(i)) diff++;
-            if(diff>1) return false;
-        }
-        return true;
     }
 }
