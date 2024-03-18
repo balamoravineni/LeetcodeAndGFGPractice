098e9d6ed5c36e127e9413819ed44470bce8ff18
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-03-18 10:38:17 +0530

    Time: 55 ms (53.68%) | Memory: 68.8 MB (65.71%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -7,19 +7,19 @@ class Solution {
         });
         // for(int i=0;i<points.length;i++) System.out.printf("[%d, %d],", points[i][0], points[i][1]);
         // System.out.println();
-        List<int[]> intersections = new ArrayList<>();
-        intersections.add(points[0]);
+        int[] prev = points[0];
+        int ans = 1;
         for(int i=1;i<points.length;i++) {
-            int[] prev = intersections.get(intersections.size()-1);
             // System.out.println(prev[1] + " < " + points[i][0] + ": " + (prev[1]<points[i][0]));
             if(prev[1]<points[i][0]) {
-                intersections.add(points[i]);
+                ans++;
+                prev = points[i];
             }
             else {
                 prev[0] = points[i][0];
                 prev[1] = Math.min(prev[1],points[i][1]);
             }
         }
-        return intersections.size();
+        return ans;
     }
 }
