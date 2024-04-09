f921c33c357585578503bd077be9a3bd0af47ee9
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-04-09 10:31:14 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (67.71%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,5 +1,20 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
+        // return bruteForce(tickets, k);
+        return optimised(tickets, k);
+    }
+
+    int optimised(int[] tickets, int k) {
+        int passes = tickets[k];
+        int ans = 0;
+        for(int i=0;i<tickets.length;i++) {
+            if(i<=k) ans += Math.min(tickets[i], passes);
+            else ans += Math.min(tickets[i], passes-1);
+        }
+        return ans;
+    }
+
+    int bruteForce(int[] tickets, int k) {
         int ans = 0;
         boolean br = false;
         while(true) {
