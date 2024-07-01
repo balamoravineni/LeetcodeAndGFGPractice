9f8389c671cb15552b46dd8742f69bfc2205adaa
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-07-01 22:14:01 +0530

    Time: 8 ms (6.78%) | Memory: 41.5 MB (6.99%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,5 +1,39 @@
 class Solution {
     public boolean check(int[] nums) {
+        // return usingTwoArraysLinear(nums);
+        return spaceOptimisedLinear(nums);
+    }
+
+    boolean spaceOptimisedLinear(int[] nums) {
+        if(nums.length<=2) {
+            return true;
+        }
+        Integer list1Lower = null; Integer list1Upper = null;
+        Integer list2Lower = null; Integer list2Upper = null;
+
+        list1Lower = nums[0];
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i]>=nums[i-1]) {
+                if(list2Lower==null) list1Upper = nums[i];
+                else list2Upper = nums[i];
+            }
+            else {
+                if(list2Lower==null) {
+                    list2Lower = nums[i];
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        System.out.println(list1Lower + " " + list1Upper);
+        System.out.println(list2Lower + " " + list2Upper);
+        list2Upper = list2Upper!=null?list2Upper:list2Lower;
+        if(list2Lower==null || list2Upper<=list1Lower) return true;
+        return false;
+    }
+
+    boolean usingTwoArraysLinear(int[] nums) {
         if(nums.length<=2) {
             return true;
         }
