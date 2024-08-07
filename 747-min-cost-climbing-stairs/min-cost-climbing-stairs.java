15ab46640bf59dc009aa7707eed097c44d5b883d
balamoravineni <95013578+balamoravineni@users.noreply.github.com>
2024-08-07 06:45:40 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (32.82%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -3,7 +3,22 @@ class Solution {
         // int[] dp = new int[cost.length+1];
         // Arrays.fill(dp,-1);
         // return recursive(-1, cost, dp);
-        return iterative(cost);
+
+        // return iterative(cost);
+        return spaceOptimized(cost);
+    }
+
+    int spaceOptimized(int[] cost) {
+        int totalCost = 0;
+        int twoSteps = cost[cost.length-1];
+        int oneStep = cost[cost.length-2];
+        for(int i=cost.length-2;i>=0;i--) {
+            int current = i>=1?cost[i-1]:0;
+            totalCost = current + Math.min(oneStep, twoSteps);
+            twoSteps = oneStep;
+            oneStep = totalCost;
+        }
+        return totalCost;
     }
 
     int iterative(int[] cost) {
